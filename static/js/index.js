231d1c4b70b6d8ab3c8e46cfc85e7fa9555c1e3b
Gabriele Di Cosmo <gabrieledicosmo05@gmail.com>
2022-10-22 16:23:09 +0200

    fiorellino rimbalzante

--- static/js/index.js
+++ static/js/index.js
@@ -1,35 +1,48 @@
 let width = 600;
 let height = 300;
+let diameter = 50;
+let deltaX = 5;
+let deltaY = 5;
+let x = 0 + diameter;
+let y = 0 + diameter;
 
 function drawFlower(x, y, d) {
-
-  let raggio = d / 2;
-  let border = 4;
-  stroke("rgb(0,0,0)");
-  strokeWeight(border);
-  fill("rgb(255, 153, 51)");
-  circle(x - raggio, y - raggio, d);
-  circle(x + raggio, y - raggio, d);
-  circle(x - raggio, y + raggio, d);
-  circle(x + raggio, y + raggio, d);
-  fill("rgb(255, 51, 0)");
-  circle(x, y, d);
+	let raggio = d / 2;
+	let border = 4;
+	stroke("rgb(0,0,0)");
+	strokeWeight(border);
+	fill("rgb(255, 153, 51)");
+	circle(x - raggio, y - raggio, d);
+	circle(x + raggio, y - raggio, d);
+	circle(x - raggio, y + raggio, d);
+	circle(x + raggio, y + raggio, d);
+	fill("rgb(255, 51, 0)");
+	circle(x, y, d);
 }
 
 function setup() {
-  createCanvas(width, height);
+	createCanvas(width, height);
 }
 
 function draw() {
+	background("green");
+	x = x + deltaX
+	y = y + deltaY
 
-  let originX = 0;
-  let diameter = 120;
-  let Xa = width / 2
-  let Ya = height / 2
-  background("rgb(0,255,0)");
+	drawFlower(x, y, diameter);
+	if (x + diameter >= width) {
+		deltaX = -deltaX;
+	}
 
-  drawFlower(Xa, Ya, diameter)
-  drawFlower(originX, Ya, diameter)
-  drawFlower(width, Ya, diameter)
+	if (x - diameter <= 0) {
+		deltaX = -deltaX;
+	}
 
+	if (y + diameter >= height) {
+		deltaY = -deltaY;
+	}
+
+	if (y - diameter <= 0) {
+		deltaY = -deltaY;
+	}
 }
